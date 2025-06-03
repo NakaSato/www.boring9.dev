87ac23597cdc882ca9c8d9fb2aa8fbb45fcbe093
WIT <wit.chanthawat@gmail.com>
2025-06-03 23:03:04 +0700

    Remove unused ContactMe import from HomeSection

--- components/sections/HomeSection.tsx
+++ components/sections/HomeSection.tsx
@@ -1,5 +1,4 @@
 import AboutMe from '../content/AboutMe';
-import ContactMe from '../content/ContactMe';
 import CurrentFavTech from '../content/CurrentFavTech';
 import CurrentLearning from '../content/CurrentLearning';
 import CurrentTimeLineExp from '../content/CurrentTimeLineExp';
